2a12d6318481167fc52fef78b27a1654b9996441
AdityaChaudhari03 <chaudhariaditya664@gmail.com>
2023-12-12 17:08:30 +0530

    origin changed

--- server/index.js
+++ server/index.js
@@ -1,58 +1,58 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
-const userRoutes = require('./routes/User')
-const profileRoutes = require('./routes/Profile')
-const paymentRoutes = require('./routes/Payment')
-const courseRoutes = require('./routes/Course')
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payment");
+const courseRoutes = require("./routes/Course");
 
-const database = require('./config/database')
-const cookieParser = require('cookie-parser')
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
 
-const cors = require('cors')
-const {cloudinaryConnect} = require('./config/cloudinary')
-const fileUpload = require('express-fileupload')
+const cors = require("cors");
+const { cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
 
 //database connect
-database.connect(); 
+database.connect();
 // //middleware
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.use(
-    cors({
-        origin:"https://studynotion-frontend-brown.vercel.app",
-        credentials:true
-    })
-) 
+  cors({
+    origin: "https://studynotionbyaditya.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use(
-    fileUpload({
-        useTempFiles:true,
-        tempFileDir:"/tmp"
-    })
-)
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
 
 //cloudinary connection
 cloudinaryConnect();
 
 //routes mount
-app.use("/api/v1/auth",userRoutes)
-app.use("/api/v1/profile",profileRoutes)
-app.use("/api/v1/course",courseRoutes)
-app.use("/api/v1/payment",paymentRoutes)
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
 
-//default route 
-app.get('/', (req, res) => {
-    return res.json({ 
-        success:true,
-        messgae:"Your server is up and running"
-    })
-    // res.send("<h1>Ye buddy light weight</h1>") 
-})
+//default route
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    messgae: "Your server is up and running",
+  });
+  // res.send("<h1>Ye buddy light weight</h1>")
+});
 
-app.listen(PORT,()=>{
-    console.log(`Your server is running on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Your server is running on ${PORT}`);
+});
